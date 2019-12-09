b65811bbc1759be0654c594ac558c68ca1d7038d
rdiehlmartinez <rdm@stanford.edu>
2019-12-09 00:54:43 -0800

    final 0.9v of notebook

--- modules/model/__init__.py
+++ modules/model/__init__.py
@@ -2,3 +2,4 @@ from modules.model.embeddings import Embeddings
 from modules.model.encoder import Encoder
 from modules.model.decoder import Decoder
 from modules.model.discriminator import Discriminator
+from modules.model.hook import Hook 
--- modules/model/discriminator.py
+++ modules/model/discriminator.py
@@ -1,5 +1,6 @@
 ''' Basic discriminator class - just one ff layer to predict language type'''
 
+import torch
 import torch.nn as nn
 import torch.nn.functional as F
 
@@ -10,16 +11,17 @@ class Discriminator(nn.Module):
         self.output_dim = output_dim
 
         self.initial_affine_size = initial_affine_size
-        if (initial_affine_size > 1):
-            self.affine_combo = nn.Linear(hidden_dim, hidden_dim//initial_affine_size)
-            self.hidden_dim = self.hidden_dim//initial_affine_size
-            self.batch_norm = nn.BatchNorm1d(hidden_dim)
+        if (initial_affine_size > 0):
+            self.affine_combo = nn.Linear(initial_affine_size, 1)
 
         self.ff_1 = nn.Linear(self.hidden_dim, self.hidden_dim //2)
         self.ff_2 = nn.Linear(self.hidden_dim //2, output_dim)
 
     def forward(self, x):
-        if self.initial_affine_size > 1:
-            x = self.batch_norm(self.affine_combo(x))
+        if self.initial_affine_size > 0:
+            # (batch_size, num_heads, hidden_dim)
+            x = torch.squeeze(self.affine_combo(x.transpose(1,2)))
+            assert(x.shape[1] == self.hidden_dim and len(x.shape) == 2), "Error in output of affine combination!"
+            # output: (batch_size, hidden_dim)
 
         return self.ff_2(F.relu(self.ff_1(x)))
